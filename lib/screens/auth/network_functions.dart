4ad7399609fcb94753f03d0d2374087db74a9c98
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-11-02 21:23:56 +0300

    Update network_functions.dart

--- lib/screens/auth/network_functions.dart
+++ lib/screens/auth/network_functions.dart
@@ -1,10 +1,17 @@
-import 'package:flutter/material.dart';
-import 'package:q_flow/supabase/supabase_visitor.dart';
+import 'dart:math';
 
+import 'package:flutter/material.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
+import 'package:q_flow/supabase/supabase_visitor.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+import '../../supabase/client/supabase_mgr.dart';
 import '../../supabase/supabase_auth.dart';
 import 'auth_cubit.dart';
 
 extension NetworkFunctions on AuthCubit {
+  static late String? userID;
+
   sendOTP(BuildContext context) async {
     try {
       emitLoading();
@@ -20,9 +27,14 @@ extension NetworkFunctions on AuthCubit {
     try {
       emitLoading();
       await SupabaseAuth.verifyOTP(emailController.text, stringOtp);
-
       await SupabaseVisitor.fetchProfile();
 
+      await OneSignal.login(Random().nextInt(99999).toString());
+      userID = await OneSignal.User.getExternalId();
+      print('userID: $userID');
+      Supabase.instance.client.from('visitor').update({
+        'external_id': userID,
+      }).eq('f_name', 'Abdullah');
       if (context.mounted) {
         if (dataMgr.visitor != null) {
           navigateToHome(context);
